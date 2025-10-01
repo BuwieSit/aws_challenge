c12896851ce1337baa8c486fb3b5b70b2dd73a69
BuwieSit <santosbuhawie28@gmail.com>
2025-10-01 22:44:42 +0800

    saan na to patungo

--- script.js
+++ script.js
@@ -22,3 +22,41 @@ cards.forEach(c => {
         blackHole.style.width = '150px';
     });
 });
+
+const faqsWrapper = document.querySelectorAll('.faqs-question-wrapper');
+
+faqsWrapper.forEach(faq => {
+    faq.addEventListener('click', () => {
+        const answer = faq.nextElementSibling; 
+
+        if (answer.style.maxHeight) {
+            answer.style.maxHeight = null;
+            answer.style.opacity = 0;
+        } else {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+            answer.style.opacity = 1;
+        }
+    });
+});
+
+const eventCard = document.querySelectorAll('.event-divider');
+const eventPopup = document.querySelector('.event-popup');
+const closeBtn = document.querySelector('.close-btn');
+
+eventCard.forEach(card => {
+    
+    card.addEventListener('click', () => {
+        console.log('clicked');
+        eventPopup.classList.remove('hide');
+        eventPopup.classList.add('show');
+    });
+
+});
+closeBtn.addEventListener('click', () => {
+
+    if (eventPopup.classList.contains('show')) {
+        eventPopup.classList.add('hide');
+        eventPopup.classList.remove('show');
+    }
+
+});
