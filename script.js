229e12a4abdc3f7677ba56097d1c0c12e124e82f
BuwieSit <santosbuhawie28@gmail.com>
2025-10-01 18:55:58 +0800

    faqs start

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const missionSect = document.querySelector('.mission');
+const aboutSect = document.querySelector('.about');
 const blackHole = document.querySelector('.blackhole');
 const cards = document.querySelectorAll('.card');
 
-missionSect.addEventListener('mousemove', function(e) {
-    const rect = missionSect.getBoundingClientRect();
+aboutSect.addEventListener('mousemove', function(e) {
+    const rect = aboutSect.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
     const size = blackHole.offsetWidth;
-
     blackHole.style.opacity = '1';
     blackHole.style.transform = `translate(${x - size / 2}px, ${y - size / 2}px)`;
 });
+aboutSect.addEventListener('mouseleave', () => {
+    blackHole.style.opacity = '0';
+})
 
 cards.forEach(c => {
     c.addEventListener('mouseenter', () => {
